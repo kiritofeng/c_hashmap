c90c5b4465f0ad2ee631684bcf1ab4af2006b96e
Benjamin Cheng <ben@bcheng.cf>
2017-10-23 20:51:43 -0400

    Update hashmap.c

--- hashmap.c
+++ hashmap.c
@@ -1,5 +1,4 @@
 #include<stdlib.h>
-#include<stdio.h>
 #define BUCKETS 100000
 struct hashmap {
     //A node in a singly-linked list
@@ -9,6 +8,7 @@ struct hashmap {
         struct node* nxt;
     } *map[BUCKETS];
 }hm;
+
 struct node* create_node(int i,void* v,struct node* n) {
     struct node* nd = (struct node*)malloc(sizeof(struct node));
     nd->key=i;
@@ -25,7 +25,7 @@ int get_bkt(int key) {
 void insert(struct hashmap *hm, int key, void* val) {
     int bkt=get_bkt(key);
     struct node **cur=&hm->map[bkt];
-    while(*cur&&(*cur)->key!=key) *cur=(*cur)->nxt;
+    while(*cur&&(*cur)->key!=key) cur=&(*cur)->nxt;
     if(*cur) (*cur)->val=val;
     else *cur=create_node(key,val,0);
 }
@@ -38,26 +38,10 @@ void* retrieve(struct hashmap *hm, int key) {
 void del(struct hashmap *hm, int key) {
     int bkt=get_bkt(key);
     struct node** cur=&hm->map[bkt];
-    while((*cur)&&(*cur)->key!=key) *cur=(*cur)->nxt;
+    while((*cur)&&(*cur)->key!=key) cur=&(*cur)->nxt;
     if(*cur) {
         struct node* tmp=*cur;
-        *cur=(*cur)->nxt;
+        cur=&(*cur)->nxt;
         free(tmp);
     }
-}
-void delete_hashmap(struct hashmap *hm) {
-    for(int i=0;i<BUCKETS;i++) {
-        node *tmp, *n=hm->map[i];
-        while(n) {
-            tmp=n;
-            n=n->nxt;
-            free(tmp);
-        }
-    }
-}
-int main() {
-    init_hashmap(&hm);
-    insert(&hm,1,1);
-    del(&hm,1);
-    printf("%d\n", (int)retrieve(&hm,1));
 }
